cddb7a4c8940d79497c02bdd5f45db29f00e04cf
Paul Sinnett <paul.sinnett@gmail.com>
2016-10-11 18:09:52 +0100

    example project files
    initial upload

---
+++ Assets/KeyMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+
+			transform.Translate (Vector3.left * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+
+			transform.Translate (Vector3.right * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+
+			transform.Translate (Vector3.forward * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+
+			transform.Translate (Vector3.back * Time.deltaTime);
+		}
+	}
+}
